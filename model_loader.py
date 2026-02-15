61bdd2702b0d3bd44c299ecf1a4580e33539464b
Bhoumik Segamkar <74896919+bhoumik12pre@users.noreply.github.com>
2026-02-15 15:00:07 +0530

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -4,21 +4,19 @@ from transformers import ViTForImageClassification
 _model = None
 
 def get_model():
-global _model
-
-```
-if _model is None:
-    print("Loading model into memory...")
-    model = ViTForImageClassification.from_pretrained(
-        "google/vit-base-patch16-224",
-        num_labels=2
-    )
-
-    state = torch.load("model/vit_face_final_best.pth", map_location="cpu")
-    model.load_state_dict(state, strict=False)
-    model.eval()
-
-    _model = model
-
-return _model
-```
+    global _model
+    
+    if _model is None:
+        print("Loading model into memory...")
+        model = ViTForImageClassification.from_pretrained(
+            "google/vit-base-patch16-224",
+            num_labels=2
+        )
+        
+        state = torch.load("model/vit_face_final_best.pth", map_location="cpu")
+        model.load_state_dict(state, strict=False)
+        model.eval()
+        
+        _model = model
+    
+    return _model
